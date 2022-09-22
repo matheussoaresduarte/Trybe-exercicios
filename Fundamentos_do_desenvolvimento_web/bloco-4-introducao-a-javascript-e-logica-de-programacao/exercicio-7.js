e39acded715122ae2a150082e725c94adff5821c
Matheus Soares <matheus.sd3210@gmail.com>
2022-09-22 12:48:22 -0300

    exercicio do imposto de renda

---
+++ Fundamentos_do_desenvolvimento_web/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicio-7.js
@@ -0,0 +1,23 @@
+//CASO UM DOS NUMEROS SEJA PAR
+
+const value1 = 1;
+const value2 = 2;
+const value3 = 3;
+let result = false;
+
+if(value1 % 2 === 0 || value2 % 2 === 0 || value3 % 2 === 0) {
+     result = true;
+     console.log("o resultado é " + result);
+}
+
+//CASO UM DOS NUMEROS SEJA IMPAR
+
+const value4 = 1;
+const value5 = 2;
+const value6 = 3;
+let result2 = false;
+
+if(value4 % 2 !== 0 || value5 % 2 !== 0 || value6 % 2 !== 0) {
+     result = true;
+     console.log("o resultado é " + result);
+}
