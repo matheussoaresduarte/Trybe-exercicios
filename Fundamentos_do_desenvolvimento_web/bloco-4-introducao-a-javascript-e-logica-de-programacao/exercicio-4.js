239299ad55d3dc36391a6c0775d9a485ab6134a7
Matheus Soares <matheus.sd32120@gmail.com>
2022-09-21 21:27:25 -0300

    exercicios de condicionais finalizados

---
+++ Fundamentos_do_desenvolvimento_web/bloco-4-introducao-a-javascript-e-logica-de-programacao/exercicio-4.js
@@ -0,0 +1,10 @@
+// COMPARANDO  2  VALORES
+
+const value1 = 10;
+const value2 = 5;
+
+if (value1 > value2) {
+    console.log("o numero " + value1 + " e maior ")
+} else{
+    console.log("o numero " + value2 + " e maior ")
+ }
