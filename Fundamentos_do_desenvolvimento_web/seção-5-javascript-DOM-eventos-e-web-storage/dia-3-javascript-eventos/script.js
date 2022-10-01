2fe691d0681a13e09131a5ec3efc3c2ef27cf652
Matheus Soares <matheus.sd3210@gmail.com>
2022-10-01 12:21:39 -0300

    criação dos dias do mes em numeros

--- Fundamentos_do_desenvolvimento_web/seção-5-javascript-DOM-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ Fundamentos_do_desenvolvimento_web/seção-5-javascript-DOM-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -14,4 +14,23 @@ function createDaysOfTheWeek() {
   createDaysOfTheWeek();
   
   // Escreva seu código abaixo.
-  
+  function createNumberDays(){
+const decemberDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+const days = document.getElementById('days');
+  for(let index = 0; index < decemberDaysList.length; index += 1){
+    const daysNumber = document.createElement("li");
+    daysNumber.innerHTML = decemberDaysList[index];
+    days.appendChild(daysNumber);
+    
+    if(decemberDaysList[index] === 24 || decemberDaysList[index]=== 25 || decemberDaysList[index] ===31){
+        daysNumber.setAttribute("class","day holiday");
+    }else if (decemberDaysList[index] === 4||decemberDaysList[index] === 11||decemberDaysList[index] === 18||decemberDaysList[index] === 25){
+        daysNumber.setAttribute("class","day friday");
+
+    }else{
+        daysNumber.className = "day";
+    }
+  }
+}
+createNumberDays();
